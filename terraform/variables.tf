9f6e542ed1b2ef918fccb7af41ce906058f71b2c
Cedric DJIVO <cedric_dji@yahoo.fr>
2024-10-12 21:07:44 +0200

    Refactor terraform variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,13 @@
 
-# variable "AWS_ACCESS_KEY_ID" {
-#   description = "Your AWS access key"
-#   type        = string
-# }
+ variable "AWS_ACCESS_KEY_ID" {
+   description = "Your AWS access key"
+   type        = string
+ }
 
-# variable "AWS_SECRET_ACCESS_KEY" {
-#   description = "Your AWS secret access key"
-#   type        = string
-# }
+ variable "AWS_SECRET_ACCESS_KEY" {
+   description = "Your AWS secret access key"
+   type        = string
+ }
 
 variable "AWS_REGION" {
   description = "The region in which AWS resources are created"
@@ -15,12 +15,12 @@ variable "AWS_REGION" {
   default     = "us-west-2" # Default region, adjust if necessary
 }
 
-# variable "SSH_PUBLIC_KEY" {
-#   description = "For SSH access"
-#   type        = string
-# }
+variable "SSH_PUBLIC_KEY" {
+  description = "For SSH access"
+  type        = string
+}
 
-# variable "AWS_SESSION_TOKEN" {
-#   description = "AWS Session Token for temporary credentials"
-#   type        = string
-# }
+variable "AWS_SESSION_TOKEN" {
+  description = "AWS Session Token for temporary credentials"
+  type        = string
+}
