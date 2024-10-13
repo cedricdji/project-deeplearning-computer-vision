1ca68e6993459e7f0439ec8fdfc6e047e492f2d3
Cedric DJIVO <cedric_dji@yahoo.fr>
2024-10-13 11:54:04 +0200

    Refactor SSH_PUBLIC_KEY default value in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,7 +19,7 @@ variable "AWS_REGION" {
 variable "SSH_PUBLIC_KEY" {
   description = "For SSH access"
   type        = string
-  default     = "" # Default value, adjust if necessary
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCpPJ6/OEvOz2nHwmXd/lycimDUv9u1ZRRbVTbdu8M2zi7Ywji1MGgiVp8t623441kUxXIsuWlJbTCWyL9Q+Ifgwk3oK8ey7JyOob1z3V3ZxSMzN2Kgv3dDnrQk5luxjzkdUOiujAiHwk2yYc+1LX3svGJIzieW52Eegufm8UosKZFrAFb5dVIxVaaoWdg6LNeYh1NqkKSw7z2AtiA/RQfiybOicgLxxD8Ea7kRMebjKQJd4M9NeeDr/ooIlb9swFvcGWhFXqPLmTQeI6VrlVPcOURTuVbJ4esE4AVtXOyU3Zxf0A3fWEZpdl+i/xJHU5HXoL8Ix5Nlwyu4S+WzvFVud6nHVi/moyU8QiDCcf82J83QvCQpExhL/QfDTtblpZ3TKN705kplOMJcBz01KD0iowNmYGCs/iO0X9OsnE//Yyk5Hb4ZpTr4ZtsA2Hd1egH740kK7o38OgowJUa99jEKIusHU3L5FYP3xCkPp965qx+hpr1MMGHRG31JptsiAVqVkp2QWMnkqfU2PzEIO3EemLQkbw/oiuHyNflA63mgH+Heq04n7ljNnfn0/CHh8vT8nYDTYQdIFDdxzbIaphGnxnA7BBgHmf9HijyrWdQx7zu5vOpbZvt2OWkcHBY3eeRIsagiW069wAXbMiYsfb/lktlB2Unc/JBt//ZN+w1xWQ== cedric_dji@yahoo.fr" # Default value, adjust if necessary
 }
 
 variable "AWS_SESSION_TOKEN" {
