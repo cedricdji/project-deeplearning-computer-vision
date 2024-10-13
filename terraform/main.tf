fc45b3689aac874f412421d903bbbfa87e2f95d8
Cedric DJIVO <cedric_dji@yahoo.fr>
2024-10-13 13:06:47 +0200

    Refactor deploy.yml to uncomment output for EC2 Public IP retrieval

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,6 +74,6 @@ resource "aws_instance" "app_server" {
 }
 
 # Output the public IP address of the EC2 instance
-# output "ec2_public_ip" {
-#   value = aws_instance.app_server.public_ip
-# }
+ output "ec2_public_ip" {
+   value = aws_instance.app_server.public_ip
+ }
