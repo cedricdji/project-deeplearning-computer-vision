564ad59f1c14cd20fc9c7ba5e149b72a73841f68
Cedric DJIVO <cedric_dji@yahoo.fr>
2024-10-14 23:57:29 +0200

    Refactor terraform output to include EC2 instance details

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,3 +2,11 @@
 output "ec2_public_ip" {
   value = aws_instance.app_server.public_ip
 }
+
+output "ec2_public_dns" {
+  value = aws_instance.app_server.public_dns
+}
+
+output "ec2_instance_id" {
+  value = aws_instance.app_server.id
+}
