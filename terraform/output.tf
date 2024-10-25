61070de32f4244afde397c4062a60a2f7b9eb7ca
Cedric DJIVO <cedric_dji@yahoo.fr>
2024-10-26 00:16:36 +0200

    Refactor Terraform output.tf to comment out unused bucket_names output

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,7 +3,7 @@ output "notebook_instance_arn" {
   value       = aws_sagemaker_notebook_instance.notebook.arn
 }
 
-output "bucket_names" {
-  description = "Noms des buckets S3 créés"
-  value       = [for bucket in aws_s3_bucket.project_buckets : bucket.bucket]
-}
+# output "bucket_names" {
+#   description = "Noms des buckets S3 créés"
+#   value       = [for bucket in aws_s3_bucket.project_buckets : bucket.bucket]
+# }
